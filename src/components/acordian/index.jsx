77eeea6e4072cc0682905af03cb4ba82bc2ae86f
filiperak <filip.erak@googlemail.com>
2024-04-20 14:16:19 +0200

    accordion test

---
+++ src/components/acordian/index.jsx
@@ -0,0 +1,64 @@
+//single selection
+//multiple selection
+
+import { useState } from "react";
+import data from "./data";
+import './style.css'
+
+const Accordian = () => {
+    const [selected,setSelected] = useState(null);
+    const [enableMulty,setEnableMulty] = useState(false);
+    const [multi,setMulti] = useState([]);
+
+    const handleSingleSelection = (currentId) => {
+        // if(currentId === selected){
+        //     setSelected(null)
+        // }else{
+        //     setSelected(currentId)
+        // }
+        setSelected(selected=== currentId ? null: currentId)
+    }
+    const handleMulti = (currentId) => {
+        let copyMulti = [...multi];
+        const findIndexOfCurrentId = copyMulti.indexOf(currentId)
+        if(findIndexOfCurrentId === -1) copyMulti.push(currentId)
+        else copyMulti.splice(findIndexOfCurrentId,1)
+        setMulti(copyMulti)
+        console.log(multi);
+
+    }
+    return(
+        <div className="wrapper">
+            <button
+            onClick={() => setEnableMulty(!enableMulty)}
+            >Enable mult selection</button>
+            <div className="accordian">
+                {
+                    data && data.length > 0 ? 
+                    data.map(elem => (
+                        <div className="item">
+                            <div 
+                            className="title"
+                            onClick={
+                                enableMulty?
+                                () => handleMulti(elem.id)
+                                :() => handleSingleSelection(elem.id)}>
+                                <h3>{elem.question}</h3>
+                                <span>+</span>
+                            </div>
+                            {
+                                selected === elem.id || 
+                                multi.indexOf(elem.id) !== -1?
+                                <div className="content">{elem.question}</div>
+                                :null
+                            }
+                        </div>
+                    ))
+                    : <div>no data found</div>
+                }
+            </div>
+        </div>
+    )
+}
+export default Accordian;
+
